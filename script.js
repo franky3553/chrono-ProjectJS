3e5cbca29e7f4047c51b5830bde3bf3003115402
franky3553 <franky3553@gmail.com>
2022-10-25 07:28:55 +0200

    Let basic elements

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 const spanMinute = document.querySelector(".minute");
 const spanSecond = document.querySelector(".second");
 const spanCentes = document.querySelector(".centes");
+
+let minute = 0;
+let second = 0;
+let centes = 0;
