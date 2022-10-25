92908603f541cba0470f86755e7d2b9addf831a5
franky3553 <franky3553@gmail.com>
2022-10-25 07:28:38 +0200

    Get basic elements

---
+++ script.js
@@ -0,0 +1,5 @@
+//1// get basic elements
+
+const spanMinute = document.querySelector(".minute");
+const spanSecond = document.querySelector(".second");
+const spanCentes = document.querySelector(".centes");
